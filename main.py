a2df72018ccf3c40a1b573830d3e40c64e26ec01
Hunter-Owings <83366080+Hunter-Owings@users.noreply.github.com>
2025-03-29 11:34:33 -0400

    Added outline to break down problem

---
+++ main.py
@@ -0,0 +1,19 @@
+# Define a Card class
+
+# Define a Deck Class
+
+# Implement the shuffle() operation
+    # Must return no value
+    # Results in cards in the deck being randomly permuted
+    # Do NOT use the library-provided shuffle function
+    # Do use the library-provided random number generator
+
+# Implement the dealOneCard() operation
+    # Returns one card from the deck to the caller
+    # A call to shuffle mused be followed by 52 calls to dealOneCard()
+    # this should result in the caller being provided all 52 cards of the deck in a random order
+    # if the caller makes a 53rd call to dealOneCard(), the caller should receive None
+
+# Must implement the principle of least surprise
+
+# Add security checks to ensure that the code meets the requirements for Appian's security standards
