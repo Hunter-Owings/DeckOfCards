c02b2adbc616575260b1babee8d208fa2c93a7af
Hunter-Owings <83366080+Hunter-Owings@users.noreply.github.com>
2025-03-30 12:10:28 -0400

    Testing the shuffle operation

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# Importing random module to use for shuffling the deck
+import random
+
 # Define a Card class
 class Card:
     
@@ -25,25 +28,37 @@ class Deck:
                 self.cards.append(Card(value, suit)) # Append the card to the deck
 
     # Define a string representation of each card in the deck
-    def __str__(self):
-        return ", ".join(str(card) for card in self.cards) 
+    # def __str__(self):
+        # return ", ".join(str(card) for card in self.cards) 
+    
+    # Implement the shuffle() operation
+    def shuffle(self):
+        # Implement the Fisher-Yates shuffle algorithm
+        for card in range(len(self.cards)-1, 0, -1): # Loops from the last card to the first card
+            rand_card = random.randint(0, card) # Picks a random card from the deck
+            self.cards[card], self.cards[rand_card] = self.cards[rand_card], self.cards[card] # Swap the current card with the randomly selected card
 
-# Testing the Card class
-# card = Card('Ace', 'Hearts') # Create a card object
-# print(card) # Print the value and suit of the card
-
-# Testing the Deck class
-deck = Deck() # Create a deck object
-print(len(deck.cards)) # Print the number of cards in the deck (should be 52)
-print(deck.cards[0]) # Print the first card in the deck
-print(deck.cards[51]) # Print the last card in the deck
-print(deck) # Print the entire deck of cards, each split by a comma
-
-# Implement the shuffle() operation
+        # Testing the shuffle operation
+        print("Shuffled deck:")
+        print(", ".join(str(card) for card in self.cards))
+    
     # Must return no value
     # Results in cards in the deck being randomly permuted
     # Do NOT use the library-provided shuffle function
     # Do use the library-provided random number generator
+
+# Testing the Deck class
+deck = Deck() # Create a deck object
+deck.shuffle() # Shuffle the deck of cards
+
+# Testing the Card class
+# card = Card('Ace', 'Hearts') # Create a card object
+# print(card) # Print the value and suit of the card
+# print(len(deck.cards)) # Print the number of cards in the deck (should be 52)
+# print(deck.cards[0]) # Print the first card in the deck
+# print(deck.cards[51]) # Print the last card in the deck
+# print(deck) # Print the entire deck of cards, each split by a comma
+
 
 # Implement the dealOneCard() operation
     # Returns one card from the deck to the caller
