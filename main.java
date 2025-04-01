671a73a26e7d3dc6270918b35e8ca50d4c6a4447
Hunter-Owings <83366080+Hunter-Owings@users.noreply.github.com>
2025-04-01 10:09:24 -0400

    making some changes and add Java file

---
+++ main.java
@@ -0,0 +1,9 @@
+// import required packages
+
+// Make Card class
+
+// Make a Deck class
+
+// Implement the shuffle() operation
+
+// Implement the dealOneCard() operation
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import random
 
 # Define a Card class
-class Card:
+class Card():
     
     # Initialize the value and suit of the card when it is created
     def __init__(self, value, suit):
@@ -19,6 +19,9 @@ class Deck:
     # Initialize the deck with 52 cards when it is created
     def __init__(self):
         self.cards = [] # Instance attribute associated with the deck of cards that is empty at the start
+        self.build_deck() # Call the build_deck method to create the deck of cards
+
+    def build_deck(self):
         suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds']  # List of suits
         values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'] # List of face values
 
@@ -34,7 +37,7 @@ class Deck:
             rand_card = random.randint(0, card) # Picks a random card from the deck
             self.cards[card], self.cards[rand_card] = self.cards[rand_card], self.cards[card] # Swap the current card with the randomly selected card
 
-# Implement the dealOneCard() operation
+    # Implement the dealOneCard() operation
     def dealOneCard(self):
         if len(self.cards) == 0: # If the deck is empty
             print("Deck is empty") # Prints if there are no cards in deck
@@ -43,8 +46,7 @@ class Deck:
             dealt_card = self.cards.pop() # Return the last card in the deck
 
         print("Card dealt: ", dealt_card) # Prints if a card is dealt
-        print("Cards left in deck: ", len(self.cards)) # Prints the number of cards left in the deck
-        
+
 # Testing the shuffle and dealOneCard operations
 deck = Deck() # Create a deck object
 deck.shuffle() # Shuffle the deck of cards
