b090fa44916f66a752d126f09a0f4e3a0686b988
Hunter-Owings <83366080+Hunter-Owings@users.noreply.github.com>
2025-04-03 08:18:12 -0400

    Restructuring comments to make them less repetitive

--- main.py
+++ main.py
@@ -1,69 +1,69 @@
-# Importing random module to use for shuffling the deck
+# Import random module to use for shuffling the deck
 import random
 
-# Define a Card class
+# Define a Card class to represent a playing card with a value and suit
 class Card():
     
-    # Initialize the value and suit of the card when it is created
+    # Cards need to have a value and a suit to be created
     def __init__(self, value, suit):
-        self.value = value # Instance attribute associated with value of the card
-        self.suit = suit   # Instance attribute associated with suit of the card
+        self.value = value
+        self.suit = suit   
 
-    # Define a string representation of the card
+    # Provides a human-readable representation of the card
     def __str__(self):
         return f"{self.value} of {self.suit}"
 
-# Define a Deck Class
+# Define a Deck Class to represent a deck of 52 playing cards that can be shuffled and dealt
 class Deck:
     
-    # Initialize the deck with 52 cards when it is created
+    # A deck of cards needs to be created with 52 cards
     def __init__(self):
-        self.cards = [] # Instance attribute associated with the deck of cards that is empty at the start
-        self.build_deck() # Call the build_deck method to create the deck of cards
-        self.current_index = 0 # Instance attribute associated with the current index of the deck
+        self.cards = [] # Stores all the cards in the deck
+        self.build_deck() # Automatically build the deck when a Deck object is created
+        self.current_index = 0 # Tracks the current index of the deck and the next card to be dealt
 
-    # Method to build the deck of cards
+    # Populate the deck with 52 cards
     def build_deck(self):
-        suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds']  # List of suits
-        values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'] # List of face values
-        print("Building deck...") # Prints if the deck is being built
+        suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'] 
+        values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+        print("Building deck...") 
 
         # Create a card for each combination of value and suit and add it to the deck
-        for suit in suits: # Loop through each suit
-            for value in values: # Loop through each value
-                self.cards.append(Card(value, suit)) # Append the card to the deck
+        for suit in suits: 
+            for value in values: 
+                self.cards.append(Card(value, suit)) 
 
-    # Implement the shuffle() operation
+    # Randomly shuffle the deck of cards using the Fisher-Yates shuffle algorithm
     def shuffle(self):
-        print("Shuffling deck...") # Prints if the deck is being shuffled
-        # Implement the Fisher-Yates shuffle algorithm
-        for card in range(len(self.cards)-1, 0, -1): # Loops from the last card to the first card
-            rand_card = random.randint(0, card) # Picks a random card from the deck
-            self.cards[card], self.cards[rand_card] = self.cards[rand_card], self.cards[card] # Swap the current card with the randomly selected card
-            self.current_index = 0 # Reset the current index to 0 after shuffling
-        print("Deck shuffled!") # Prints if the deck is shuffled
+        print("Shuffling deck...")
+        for card in range(len(self.cards)-1, 0, -1): # Iterate through the deck in reverse order
+            rand_card = random.randint(0, card) # Get a random index from 0 to the current card index
+            self.cards[card], self.cards[rand_card] = self.cards[rand_card], self.cards[card] # Swap the current card with the random card
+            self.current_index = 0 # Reset dealing index to 0 after shuffling for a fresh start
+        print("Deck shuffled!") 
 
-    # Implement the dealOneCard() operation
+    # Retrieve the next card from the deck and check to see if the deck is empty
     def dealOneCard(self):
-        if self.current_index >= len(self.cards): # If the deck is empty
-            print("No card dealt, deck is empty!") # Prints if there are no cards in deck
-            return None # Returns None
-        else: # If there are cards in the deck
-            dealt_card = self.cards[self.current_index] # Return the card at the current index
-            self.current_index += 1 # Increment the current index
-            print("Card dealt: ", dealt_card) # Prints if a card is dealt
-            return dealt_card # Returns the dealt card
+        if self.current_index >= len(self.cards): # To ensure that we don't deal more cards than available in the deck
+            print("No card dealt, deck is empty!")
+            return None 
+        else: 
+            dealt_card = self.cards[self.current_index] # Get the next card from the deck
+            self.current_index += 1 # Increment the current index for next deal
+            print("Card dealt: ", dealt_card) 
+            return dealt_card 
     
-    # Method to return the number of cards left in the deck
+    # Determine the number of cards left in the deck
     def cards_left(self):
-        return len(self.cards) - self.current_index # Returns the number of cards left in the deck
+        return len(self.cards) - self.current_index
 
 deck = Deck() # Create a deck object
-deck.shuffle() # Shuffle the deck of cards
-for card in range(52): # Loop through each card in the deck
-    deck.dealOneCard() # Deal one card from the deck
+deck.shuffle() # Shuffle the deck of cards before dealing
 
-print("Cards left in deck: ", deck.cards_left()) # Prints the number of cards left in the deck
+# Deal 52 cards from the deck
+for card in range(52): 
+    deck.dealOneCard()
 
-deck.dealOneCard() # Should return None with print statement since the deck is empty now
+print("Cards left in deck: ", deck.cards_left()) # Confirm that the deck is empty after dealing all cards
 
+deck.dealOneCard() # Attempt to deal one more card from the deck after all cards have been dealt
